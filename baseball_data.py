4dfd804eafa4fabeac1ea59b9dc3393ead062a94
Jason Zhou <jasonzhou123@hotmail.com>
2024-03-27 22:19:06 -0700

    add pitch data for every pitch between 2017 and 2023

--- baseball_data.py
+++ baseball_data.py
@@ -9,10 +9,15 @@ from pybaseball import cache
 cache.enable()
 
 def main():
-    data = statcast(start_dt='2017-04-02', end_dt='2023-10-29')
-    data.to_csv('statcast-data.csv', index = False, compression = 'gzip')
-    # kershaw_id = playerid_lookup('kershaw', 'clayton')
-    # print(kershaw_id['key_mlbam'])
+    # This was the original code that was used to download the data and stored in a .csv.gz file
+    # The file was over 600MB, so it was not included in the repository
+    #data = statcast(start_dt='2017-04-02', end_dt='2023-10-29')
+    
+    # Get the columns that we're interested in from the original data
+    data = pd.read_csv('statcast-data.csv.gz')
+    data = data[['pitch_type', 'release_speed', 'pitcher', 'home_team', 'away_team', 'pitch_name', 'release_spin_rate']]
+    data.to_csv('statcast-data-filtered.csv.gz', index = False, compression = 'gzip')
+
 
 if __name__ == '__main__':
     main()
