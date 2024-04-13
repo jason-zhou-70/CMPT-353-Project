c3877151b90078fb77670a44975a376ffb880c72
Jason Zhou <jasonzhou123@hotmail.com>
2024-04-13 13:20:25 -0700

    modify to loop through data folders

--- game_time_temperatures.py
+++ game_time_temperatures.py
@@ -2,6 +2,27 @@ import numpy as np
 import pandas as pd
 import os
 
+cities = ['anaheim',
+          'st-louis',
+          'new-york',
+          'philadelphia',
+          'detroit',
+          'denver',
+          'los-angeles',
+          'boston',
+          'cincinnati',
+          'chicago',
+          'kansas-city',
+          'washington',
+          'oakland',
+          'san-francisco',
+          'baltimore',
+          'san-diego',
+          'pittsburgh',
+          'cleveland',
+          'minneapolis',
+          'atlanta']
+
 # Function to convert Fahrenheit to Celsius
 def f_to_c(f):
     return (f - 32) * 5/9
@@ -15,38 +36,24 @@ def process_data(game_time_input, weather_input):
     converted_tavg = lowest_tavg_by_date.apply(f_to_c)
     converted_tavg.name = 'TAVG'
     
-    merged_data = game_time_input.merge(converted_tavg, left_on = 'date', right_on = 'DATE')
+    merged_data = game_time_input.merge(converted_tavg, left_on = 'date', right_index = True)
     
     return merged_data
 
 def main():
-    seperate_game_time_path = './seperate_game_time/'
-    seperate_weather_path = './weather_data/'
-
-    seperate_game_time = [f for f in os.listdir(seperate_game_time_path) if f.endswith('.csv')]
-    seperate_weather = [f for f in os.listdir(seperate_weather_path) if f.endswith('.csv')]
-
-    seperate_game_time_df = []
-    for file in seperate_game_time:
-        file_path = os.path.join(seperate_game_time_path, file)
-        df = pd.read_csv(file_path)
-        seperate_game_time_df.append(df)
-
-    seperate_weather_df = []
-    for file in seperate_weather:
-        file_path = os.path.join(seperate_weather_path, file)
-        df = pd.read_csv(file_path)
-        seperate_weather_df.append(df)
     
     joined_data = pd.DataFrame(columns = ['venue_name', 'date', 'time', 'TAVG'])
-    for i in range(1,21):
-        game_time = seperate_game_time_df[i]
-        weather = seperate_weather_df[i]
+    
+    for city in cities:
+        game_time = pd.read_csv(f'./game_time_data/{city}-game-time.csv')
+        weather = pd.read_csv(f'./weather_data/{city}.csv')
         merged_data = process_data(game_time, weather)
         joined_data = pd.concat([joined_data, merged_data])
     
+    joined_data = joined_data[['venue_name', 'date', 'time', 'TAVG']]
+    
     # Save to CSV
-    merged_data.to_csv('game_time_temperatures.csv', index = False)
+    joined_data.to_csv('game_time_temperatures.csv', index = False)
 
 if __name__ == '__main__':
     main()
